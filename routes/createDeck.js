48d89583fd015c497802592681f3a7fc04911c38
Gabriela Kadzielawa <gabrielakadzielawa@Gabrielas-MacBook.local>
2022-04-30 13:43:50 -1000

    create deck post route

--- routes/createDeck.js
+++ routes/createDeck.js
@@ -7,23 +7,22 @@ const Card = require('../models/Card')
 
 // GET ABOUT PAGE
 
-router.get('/', authenticateLogin, restrictRoute, function(req, res, next) {
-
-    // TEST PLATFORM FOR SCHEMAS AND MONGO
-    // NEW DECK
-
-    const deck = new Deck()
-    deck.deckName = "Cat Deck"
-    deck.deckDescription = "A deck for cats and cat-like pets."
-    deck.deckImage = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.E-8dUX4RrcU_kYtNfFg4dAHaHa%26pid%3DApi&f=1'
-    deck.creator = res.id
-    deck.tags = ['cats', 'furr', 'service animals']
-    deck.cards = []
-    deck.ratings = [1,1,3]
-    deck.save()
-
-    res.render('createDeck', { loggedIn: res.authenticate, username: res.username });
+router.get('/', authenticateLogin, restrictRoute, async function(req, res, next) {
+    const cards = await Card.find({}).lean()
+    res.render('createDeck', { cards: cards, loggedIn: res.authenticate, username: res.username });
 });
+
+router.post('/', authenticateLogin, restrictRoute, async function(req, res, next) {
+    const deck = new Deck(req.body);
+    deck.save((err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("DECK SAVED!")
+        }
+    })
+    res.redirect('/')
+})
 
 module.exports = router;
 
